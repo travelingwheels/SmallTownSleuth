04e786f126a99c42781db91df29d325df6062f97
travelingwheels <gkwheeler40@gmail.com>
2020-07-23 22:28:50 -0600

    create review migration

---
+++ db/migrate/20200724042614_create_reviews.rb
@@ -0,0 +1,9 @@
+class CreateReviews < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reviews do |t|
+      t.text :content
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :town, null: false, foreign_key: true
+    end
+  end
+end
