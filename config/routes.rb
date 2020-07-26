3fc68c96f1e1f18f162ec2471381600174ab014b
travelingwheels <gkwheeler40@gmail.com>
2020-07-26 02:37:44 -0600

    add routes for town, user, review, and comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :towns
+  resources :users
+  resources :reviews
+  resources :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
