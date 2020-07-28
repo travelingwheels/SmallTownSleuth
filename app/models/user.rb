d734eee6ce8e70c884f05eee2b5d93ca82d22e74
travelingwheels <gkwheeler40@gmail.com>
2020-07-27 19:26:27 -0600

    install yarn and webpacker to rails v6

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :reviews
   has_many :comments
-  has_many :commented_reviews, through: :comments, scource: :review
+  has_many :commented_reviews, through: :comments, source: :review
   has_many :towns, through: :reviews
 
 end
