40b08f3d3e9704aa5c3ebfa6aa1b1e386e25c83e
travelingwheels <gkwheeler40@gmail.com>
2020-07-27 20:45:46 -0600

    add post route to routes, and define create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,15 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      #log user in
+      session[:user_id] = @user.id
+      #redirect to the show page
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def show
