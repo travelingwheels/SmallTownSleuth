976158c405c74d1850496e4594a0266ec6c5a611
travelingwheels <gkwheeler40@gmail.com>
2020-07-27 20:32:37 -0600

    add strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,16 @@ class UsersController < ApplicationController
   end
 
   def create
+
   end
 
   def show
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
