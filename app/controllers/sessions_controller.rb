7ff788db6233cf7b977358f63d42dcb5d858d46a
travelingwheels <gkwheeler40@gmail.com>
2020-07-28 23:26:20 -0600

    add destroy and create methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,22 @@
 class SessionsController < ApplicationController
 
-  def destroy
-    session.destroy
+  def home
   end
 
-  def new
+  def destroy
+    session.clear
+    redirect_to root_path
   end
 
   def create
+    user = User.find_by(email: params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      redirect_to "/login"
+    end
   end
+
+
 end
