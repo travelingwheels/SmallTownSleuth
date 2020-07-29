206b77b45238c7977cb24d19314c71344620c37e
travelingwheels <gkwheeler40@gmail.com>
2020-07-28 23:47:59 -0600

    add helper method macro, define redirect_if_not_logged_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
 
-
+private 
   def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
@@ -9,5 +10,7 @@ class ApplicationController < ActionController::Base
     !!current_user
   end
 
-  
+  def redirect_if_not_logged_in
+    redirect_to '/' if !logged_in?
+  end
 end
