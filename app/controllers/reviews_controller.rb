c29170015f9ca573ba7097bb2aa7cb933bef1e65
travelingwheels <gkwheeler40@gmail.com>
2020-07-30 18:52:03 -0600

    create 'create' method for review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,15 @@ class ReviewsController < ApplicationController
   before_action :redirect_if_not_logged_in
 
   def new
-    @post = Post.new
+    @Review = Review.new
+  end
+
+  def create
+    @review = current_user.reviews.build(review_params)
+    if @review.save
+      redirect_to reviews_path
+    else
+      render :new
+    end
   end
 end
