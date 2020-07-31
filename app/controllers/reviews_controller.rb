71f03e126a98f4f109498ce74dd3b34d6441acba
travelingwheels <gkwheeler40@gmail.com>
2020-07-31 15:47:53 -0600

    fix typo in review new and create method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,13 +2,13 @@ class ReviewsController < ApplicationController
   before_action :redirect_if_not_logged_in
 
   def new
-    @Review = Review.new
+    @review = Review.new
   end
 
   def create
     @review = current_user.reviews.build(review_params)
     if @review.save
-      redirect_to reviews_path
+      redirect_to review_path
     else
       render :new
     end
