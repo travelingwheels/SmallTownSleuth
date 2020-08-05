c1b1346844a85a22ace50b8fd4faf4918ed251f0
travelingwheels <gkwheeler40@gmail.com>
2020-08-04 21:03:52 -0600

    add methods new, create, index, show and add strong params

--- app/controllers/towns_controller.rb
+++ app/controllers/towns_controller.rb
@@ -1,2 +1,32 @@
 class TownsController < ApplicationController
+  before_action :redirect_if_not_logged_in
+
+  def new
+    @town = Town.new
+  end
+
+  def create
+    @town = current_user.towns.build(town_params)
+    if @town.save
+      redirect_to towns_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @towns = Town.all#.includes(:reviews)
+  end
+
+  def show
+     @town = Town.find_by_id(params[:id])
+  end
+
+  private
+
+  def town_params
+    params.require(:town).permit(:name)
+  end
+
+
 end
