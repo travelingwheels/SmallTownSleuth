993d26c9580366d1a874428382c95af6026254e6
travelingwheels <gkwheeler40@gmail.com>
2020-08-04 19:16:17 -0600

    change attributes

--- db/migrate/20200724041322_create_towns.rb
+++ db/migrate/20200724041322_create_towns.rb
@@ -2,10 +2,8 @@ class CreateTowns < ActiveRecord::Migration[6.0]
   def change
     create_table :towns do |t|
       t.string :name
-      t.string :shopping
-      t.string :dinning
-      t.string :attraction
-      t.string :lodging
+      t.string :state
+      t.string :amenities
 
       t.timestamps
     end
