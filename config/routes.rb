92c08d78a6b00cdb6b3a55208064332cdc50a653
travelingwheels <gkwheeler40@gmail.com>
2020-08-04 21:11:29 -0600

    first attempt at nesting routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,16 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new'
   post 'login' => 'sessions#create'
-  
+
   delete '/logout' => 'sessions#destroy'
 
 
-  resources :towns
-  resources :users
+  resources :towns do
+    resources :reviews
+  end
+  resources :users do
+    resources :reviews
+  end
   resources :reviews
   resources :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
