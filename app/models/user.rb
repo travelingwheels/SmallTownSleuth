5292fc551cdaa877939636b57614617598b16c55
travelingwheels <gkwheeler40@gmail.com>
2020-08-04 19:20:33 -0600

    change associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :reviews
   has_many :comments
-  has_many :commented_reviews, through: :comments, source: :review
+  has_many :reviews
   has_many :towns, through: :reviews
 
 end
