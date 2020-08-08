0ddc07a6d62739bd5e3b5c346354d613bf132631
travelingwheels <gkwheeler40@gmail.com>
2020-08-08 17:34:46 -0600

    add fake data to work with

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+#include 'Faker'
+
+5.times do
+  Town.create(
+    name: Faker::Address.city,
+    state: Faker::Address.state_abbr
+  )
+end
