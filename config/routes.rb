d9e2faac06adb933968501b2ae2b949ea4e27904
travelingwheels <gkwheeler40@gmail.com>
2020-08-13 21:36:56 -0600

    nest review resources to town resources

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,9 @@ Rails.application.routes.draw do
   resources :towns do
     resources :reviews
   end
-  resources :users do
-    resources :reviews
-  end
+  resources :users #do
+    #resources :reviews
+  #end
   resources :reviews
   resources :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
