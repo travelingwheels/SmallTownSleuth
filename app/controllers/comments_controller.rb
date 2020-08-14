36ba97beedd9085badbeb888965294280d06d23a
travelingwheels <gkwheeler40@gmail.com>
2020-08-13 21:52:59 -0600

    delete all actions in this controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,4 @@
 class CommentsController < ApplicationController
 
-  def new
-    @comment = Comment.new
-  end
-
-  def create
-    @comment = current_user.comments.build(comment_params)
-    if @comment.save
-      redirect_to comments_path
-    else
-      render :new
-    end
-  end
-
-  def index
-    @comments = Comment.all
-  end
-
-
-  private
-
-  def comment_params
-    params.require(:comment).permit(:content)
-  end
+  
 end
