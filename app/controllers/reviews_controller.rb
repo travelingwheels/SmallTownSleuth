848e5c454236251ab7862020b50b9a6b3ab7491d
travelingwheels <gkwheeler40@gmail.com>
2020-08-13 21:51:54 -0600

    add and delete code in attempt to get nested form to work

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,17 +2,20 @@ class ReviewsController < ApplicationController
   before_action :redirect_if_not_logged_in
 
   def new
-    if params[:user_id] && @user = User.find_by_id([:user_id])
-      @review = @user.reviews.build
-    else
+    #if params[:user_id] && @user = User.find_by_id([:user_id])
+      #@review = @user.reviews.build
+      #@review.build_town
+    #else
       @review = Review.new
-    end
+      @review.build_town
+    #end
   end
 
   def create
-    @review = current_user.reviews.build(review_params)
+    #raise params.inspect
+    @review = Review.new(review_params)#current_user.reviews.build(review_params)
     if @review.save
-      redirect_to review_path
+      redirect_to review_path(@review)
     else
       render :new
     end
@@ -20,17 +23,17 @@ class ReviewsController < ApplicationController
 
   def index
     @reviews = Review.all
-    #@reviews = Reviews.includes(:user).all
+    #@reviews = Review.includes(:user).all
   end
 
   def show
     @review = Review.find_by_id(params[:id])
-    redirect_to reviews_path if !@review
+    redirect_to town_review_path if !@review
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :user_id, :town_id, town_attributes:[:name, :state, :amenities])
   end
 end
