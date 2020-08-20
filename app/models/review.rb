33fcdca4f007d7790036376fadfb71bfa3572236
travelingwheels <gkwheeler40@gmail.com>
2020-08-19 20:18:28 -0600

    define method for nested attributes

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,10 @@ class Review < ApplicationRecord
   belongs_to :town
   has_many :comments
 
-  accepts_nested_attributes_for :town
+  #accepts_nested_attributes_for :town
+
+  def town_attributes=(town_params)
+   town = Town.find_or_create_by(town_params)
+   town.valid? ? self.town = town : self.town
+  end
 end
