37124005799fda0fcd4ade6b25db4fc6b14a6c87
travelingwheels <gkwheeler40@gmail.com>
2020-08-25 20:15:48 -0600

    add new, create, index and show actions with logic

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,49 @@
 class CommentsController < ApplicationController
+    before_action :redirect_if_not_logged_in
 
-  
+    def new
+        if params[:review_id] && @review = Review.find_by_id(params[:review_id])
+            @comment = @review.comments.build
+        else
+            @comment = Comment.new
+            @comment.build_review
+        end
+    end
+    
+    def create
+        if params[:review_id] && @review = Review.find_by_id(params[review_id])
+            @comment = @review.comments.build(comment_params)
+        else
+            @comment = Comment.new(comment_params)
+            #raise params.inspect
+        end
+      
+        if @comment.save
+            redirect_to review_comments_path(@comment)
+        else
+            render :new
+       end
+    end
+
+    def index
+        if params[:review_id] && @review = Review.find_by_id(params[:review_id])
+            @comments = @review.comments
+            #raise params.inspect
+        else
+            @comments = Comment.all
+        end
+    end
+
+
+    def show
+      @comment = Comment.find_by_id(params[:id])
+    end
+
+
+    private
+
+        def comment_params
+            params.require(:comment).permit(:content, :review_id, :user_id)
+        end
+
 end
