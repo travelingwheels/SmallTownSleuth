5930df7ed6e0832fe6bf8c276ba9f97fa48a5d43
travelingwheels <gkwheeler40@gmail.com>
2020-09-12 01:24:44 -0600

    add two methods to capitalize data

--- app/models/town.rb
+++ app/models/town.rb
@@ -1,12 +1,21 @@
 class Town < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :users, through: :reviews
+  before_save :capitalize_name, :titleize_state
 
   validates :name, :state, presence: true
 
   scope :order_by_state, -> { order(:state) }
-
+  
   def formatted_name
     "#{name}, #{state}"
+  end
+
+  def capitalize_name
+    self.name = self.name.split.collect(&:capitalize).join(' ') if self.name && !self.name.blank?
+  end
+
+  def titleize_state
+    self.state = self.state.split.collect(&:upcase).join(' ') if self.state && !self.state.blank?
   end
 end
